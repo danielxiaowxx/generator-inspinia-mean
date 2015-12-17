1fc59af35c0712311b3df00c200a38a144a803ef
danielxiaowxx <xiaohutu136@163.com>
2015-12-17 14:54:14 +0800

    add mysqldb and mongodb

---
+++ generators/add-module/templates/server/daos/_.server.dao.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var path = require('path');
+
+var gmMysql = require(path.resolve('./modules/core/server/common/gmMysql'));
+var gmMongo = require(path.resolve('./modules/core/server/common/gmMongo'));
