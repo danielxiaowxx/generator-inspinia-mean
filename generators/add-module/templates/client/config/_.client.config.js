b2d6ae9097361345eed48d6c6a70da00f1d5e99f
danielxiaowxx <xiaohutu136@163.com>
2016-02-29 11:17:24 +0800

    upgrade

--- generators/add-module/templates/client/config/_.client.config.js
+++ generators/add-module/templates/client/config/_.client.config.js
@@ -1,12 +1,23 @@
 'use strict';
 
 // Configuring the <%= firstCapCamelModuleName %> module
-angular.module('<%= moduleName %>').run(['Menus',
-  function (Menus) {
+angular.module('<%= moduleName %>').run(['Menus', 'i18n',
+  function (Menus, i18n) {
+
+    function getTitle() {
+      var titleI18n = {
+        en: '<%= firstCapCamelModuleName %>英文',
+        zh_CN: '<%= firstCapCamelModuleName %>中文'
+      };
+      return titleI18n[i18n.getCurrentLang()];
+    }
+
     // Add the <%= moduleName %> dropdown item
     Menus.addMenuItem('sideBar', {
-      title: '<%= firstCapCamelModuleName %>',
+      title: getTitle(),
       state: '<%= moduleName %>.list',
+      //position: 1, // 用于显示该菜单的排序位置
+      //iconClass: '', // 菜单的ICON样式
       //type: 'dropdown', // 有子菜单时把注释去掉
       roles: ['*']
     });
@@ -20,5 +31,5 @@ angular.module('<%= moduleName %>').run(['Menus',
 ]);
 
 angular.module('<%= moduleName %>').constant('<%= firstCapCamelModuleName %>Const', {
-  I18N: window.<%= moduleName %>_i18n
+
 });
